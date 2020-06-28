423e6222cfd3b6fdb35b3f9f4306de3bafb3f3a5
sharmanvarun <39104659+sharmanvarun@users.noreply.github.com>
2020-06-29 01:59:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from hotel import Hotel
 hotel_list = []
 def create_hotel():
-    hotel_name = input("Enter Hotel Name \n")
-    hotel_list.append(Hotel(hotel_name))
+    print("Existing Hotel List")
+    print("==========================================================")
+    hotel_names = []
+    for hotel in hotel_list:
+        hotel_names.append(hotel.get_hotel_name())
+        print(hotel.get_hotel_name())
+    print("===========================================================")
+    hotel_name = input("Enter Hotel Name \n")    
+    if hotel_name in hotel_names:
+        print("Hotel already exists")
+    else:
+        hotel_list.append(Hotel(hotel_name))
 
 def add_hotel_room():
     print("Existing Hotel List")
@@ -19,26 +29,34 @@ def add_hotel_room():
         print("===========================================================")
         return    
     try:
-        room_number = int(input("Enter Room Number \n"))
-    
-        items = dict()
-        while True:
-            item_name = input("Enter Item Name : \n")
-            try:
-                item_price = float(input("Enter Item Price (in $): \n"))
-                items[item_name] = item_price
-            except ValueError:
-                print("Enter Valid Price")
-            
-            user_choice = input("Add another item? (Y/N)  ")
-            if user_choice == 'N' or user_choice == 'n':
-                break
-        print("Adding")
         for hotel in hotel_list:
+           if hotel.get_hotel_name() == hotel_name:
+                room_names =[]
+                for room in hotel.get_all_rooms():
+                    room_names.append(room.get_room_number())
+                print("Room Numbers already there : {}".format(room_names))
+        room_number = int(input("Enter Room Number \n"))    
+        if room_number in room_names:
+            print ("room already exists")
+        else:
+            items = dict()
+            while True:
+                item_name = input("Enter Item Name : \n")
+                try:
+                    item_price = float(input("Enter Item Price (in $): \n"))
+                    items[item_name] = item_price
+                except ValueError:
+                    print("Enter Valid Price")
+                
+                user_choice = input("Add another item? (Y/N)  ")
+                if user_choice == 'N' or user_choice == 'n':
+                    break
+            print("Adding")
+            for hotel in hotel_list:
 
-            if hotel.get_hotel_name() == hotel_name:
-                print("Added")
-                hotel.add_room(room_number, items)
+                if hotel.get_hotel_name() == hotel_name:
+                    print("Added")
+                    hotel.add_room(room_number, items)
     except ValueError:
         print("Room Number cannot be a string")
 
@@ -96,32 +114,32 @@ while True:
     print("4. Rooms under budget")
     print("5. Exit")
 
-
-    choice = int(input("Enter the desired choice \n"))
-
-    if choice == 1:
-        create_hotel()
-    elif choice == 2:
-        if len(hotel_list)>0:
-            add_hotel_room()
+    try:
+        choice = int(input("Enter the desired choice \n"))
+        if choice == 1:
+            create_hotel()
+        elif choice == 2:
+            if len(hotel_list)>0:
+                add_hotel_room()
+            else:
+                print("No Hotel present")
+        elif choice == 3:
+            if len(hotel_list)>0:
+                print_all_rooms()
+            else:
+                print("No Hotel present")
+        elif choice == 4:
+            if len(hotel_list)>0:
+                print_rooms_under_budget()
+            else:
+                print("No Hotel present")
+        elif choice == 5:
+            print("Byee")
+            break
         else:
-            print("No Hotel present")
-    elif choice == 3:
-        if len(hotel_list)>0:
-            print_all_rooms()
-        else:
-            print("No Hotel present")
-    elif choice == 4:
-        if len(hotel_list)>0:
-            print_rooms_under_budget()
-        else:
-            print("No Hotel present")
-    elif choice == 5:
-        print("Byee")
-        break
-    else:
-        print("Invalid Choice")
-        
+            print("Invalid Choice")
+    except ValueError:
+        print("Enter valid choice please")    
         
 
 
